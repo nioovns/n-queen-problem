2de016022f717519deb0138ecf7252c6847584e4
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-05-19 00:29:29 +0330

    adjusted queen positions on the chessboard

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,25 +1,3 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-
-# class ChessBoard:
-#     def __init__(self, n, queens):
-#         self.n = n
-#         self.queens = queens
-#         self.draw_board(queens)
-
-#     def draw_board(self, queens):
-#         board = np.add.outer(range(self.n), range(self.n)) % 2
-#         plt.imshow(board, cmap="binary_r")
-
-#         # drawing queens with red dots
-#         for row, col in enumerate(queens):
-#             plt.plot(col - 1 , row, 'ro', markersize=20) 
-#         plt.xticks([])
-#         plt.yticks([])
-#         plt.title(f"{self.n}-Queens Solution")
-#         plt.show()
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -37,7 +15,6 @@ class ChessBoard:
 
         # drawing queens
         for row, col in enumerate(queens):
-            # plt.text(col - 1.28, row + 0.25, u'\u265B', fontsize=fontSize, color='red', zorder=10)
             plt.text(col - 1, row + 0.1, u'\u265B', fontsize=fontSize, color='red', ha='center', va='center')
 
         plt.xticks([])
