7e3fa511128a020417c3d72930dcc5ae8e2eb257
newsha varnaseri <nioshavarnaseri@gmail.com>
2025-05-26 19:20:09 +0330

    csp with MRV heuristic

--- csp.py
+++ csp.py
@@ -1,83 +1,64 @@
-from constraint import Problem, AllDifferentConstraint
+
 
 class CSP:
     def __init__(self, n):
         self.n = n
-        self.sol = [-1] * n
-        self.domains =  [list(range(self.n)) for _ in range(self.n)]
-
-        # self.problem = Problem()
-        # self._setup()
-
-    # def _setup(self):
-    #     cols = range(self.n)
-    #     self.problem.addVariables(cols, range(self.n))
-    #     self.problem.addConstraint(AllDifferentConstraint())
-
-    #     for col1 in cols:
-    #         for col2 in cols:
-    #             if col1 < col2:
-    #                 self.problem.addConstraint(
-    #                     lambda q1, q2, c1=col1, c2=col2: abs(q1 - q2) != abs(c1 - c2),
-    #                     (col1, col2)
-    #                 )
+        self.variables = list(range(n))  #row
+        self.domains = {var: list(range(n)) for var in self.variables} # for every row, all cols that you can adjust a queen 
+        self.assignment = {}
      
-    def isConsidtant(self, sol, row, col):
-        for r in range(row):
-            c = sol[r]
-            if c == col or abs(c - col) == abs(r - row):
+     
+    def isConsitent(self, var, val):
+        for other_var in self.assignment:
+            other_val = self.assignment[other_var]
+            if val == other_val or abs(var - other_var) == abs(val - other_val):
                 return False
-        return True  
-    
-    
-    # def forwardChecking(self, sol, n, row, domains):
-    #     placedCol = sol[row]
-    #     for r in range(row + 1, n):
-    #         newDomains = []
-    #         for c in domains[r]:
-    #             if self.isConsidtant(sol, r, c):
-    #                 newDomains.append(c)
-    #         if not newDomains:
-    #             return False
-    #         domains[r] = newDomains
-    #     return True
-    def forwardChecking(self, sol, n, row, domains):
-        placed_col = sol[row]
-        for r in range(row + 1, n):
-            newDomains = []
-            for c in domains[r]:
-                # فقط بررسی با queen فعلی در (row, placed_col)
-                if c != placed_col and abs(placed_col - c) != abs(row - r):
-                    newDomains.append(c)
-            if not newDomains:
-                return False
-            domains[r] = newDomains
         return True
+    
+    
+    def forwardChecking(self, var, val, domains):
+        newDomain = {v: list(domains[v]) for v in domains}
+        for other_var in self.variables:
+            if other_var not in self.assignment and val in newDomain[other_var]:
+                if var == other_var:
+                    continue
+                try:
+                    newDomain[other_var].remove(val)
+                except ValueError:
+                    pass
+                diag1 = val + (other_var - var)
+                diag2 = val - (other_var - var)
+                for d in [diag1, diag2]:
+                    if d in newDomain[other_var]:
+                        newDomain[other_var].remove(d)
+                if not newDomain[other_var]: 
+                    return None
+        return newDomain
+    
+    
+    def selectUnassignedVar(self, domains):
+        unassigned = [v for v in self.variables if v not in self.assignment]
+        return min(unassigned, key=lambda var: len(domains[var]))
+            
+            
+    def backtrack(self, domains):
+        if len(self.assignment) == self.n:
+            return self.assignment
 
-            
-    def backTrack(self, sol, n, row, domains):
-        if row == n:
-            return sol
-            
-        for col in domains[row]:
-            if self.isConsidtant(sol, row, col):
-                sol[row] = col
-                newDomains = [list(m) for m in domains]
-                if self.forwardChecking(sol, n, row, newDomains):
-                    result = self.backTrack(sol, n, row+1, newDomains)
+        var = self.selectUnassignedVar(domains)
+        for val in domains[var]:
+            if self.isConsitent(var, val):
+                self.assignment[var] = val
+                newDomain = self.forwardChecking(var, val, domains)
+                if newDomain is not None:
+                    result = self.backtrack(newDomain)
                     if result:
                         return result
-                sol[row] = -1
+                del self.assignment[var]
         return None
-    
                     
     def solve(self):
-        result = self.backTrack(self.sol, self.n, 0, self.domains)
+        result = self.backtrack(self.domains)
         if result:
-            return [x+1 for x in result] 
-        
-        # return [result[i] + 1 for i in range(self.n)]
-        # sol = self.problem.getSolution()
-        # if not sol:
-        #     return [-1]
-        # return [sol[i] + 1 for i in range(self.n)]
+            return [result[i] + 1 for i in range(self.n)]
+        return [-1]   
